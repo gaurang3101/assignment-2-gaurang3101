ef9faf3c9b991df487f0d2a04a094e419ad01d7e
Gaurang <gaurang3101@gmail.com>
2020-03-28 19:46:17 +0530

    created fields root annd countRight

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -3,4 +3,8 @@ package problem1.mybst;
 import problem1.node.TreeNode;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    private TreeNode root;
+    private int countRight;
+
+
 }
