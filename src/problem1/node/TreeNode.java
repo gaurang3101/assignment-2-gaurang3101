36ee36578773368d67b4743f7d291ca59169cffe
Gaurang <gaurang3101@gmail.com>
2020-03-28 19:24:46 +0530

    Constructor

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -10,4 +10,10 @@ public class TreeNode {
     private TreeNode left;
     private TreeNode right;
     private int data;
+
+    public TreeNode(TreeNode left, TreeNode right, int data) {
+        this.left = left;
+        this.right = right;
+        this.data = data;
+    }
 }
