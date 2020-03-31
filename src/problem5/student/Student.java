f6fbf85bd1f65083a4f689dfb2fda27f687ce530
Gaurang <gaurang3101@gmail.com>
2020-03-31 20:46:35 +0530

    Problem 5, Student class getter and setters made.

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -11,5 +11,47 @@ public class Student {
         this.name = name;
     }
 
+    public Student(int rollNo, String name, int backLogCounter, int appearingCount) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.backLogCounter = backLogCounter;
+        this.appearingCount = appearingCount;
+    }
 
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " #" + rollNo;
+    }
+
+    public int getBackLogCounter() {
+        return backLogCounter;
+    }
+
+    public void setBackLogCounter(int backLogCounter) {
+        this.backLogCounter = backLogCounter;
+    }
+
+    public int getAppearingCount() {
+        return appearingCount;
+    }
+
+    public void setAppearingCount(int appearingCount) {
+        this.appearingCount = appearingCount;
+    }
 }
