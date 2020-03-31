659fb6205b2e3cccd7d2fa890eb296346b86d063
Gaurang <gaurang3101@gmail.com>
2020-03-31 20:14:46 +0530

    Problem1, Final Commit

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -6,21 +6,9 @@ import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree binarySearchTree = new MyBinarySearchTree();
-        /*for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 6; i++) {
             binarySearchTree.insert((int)(Math.random()*100));
-            //binarySearchTree.insert(i+1);
-        }*/
-        binarySearchTree.insert(83);
-        binarySearchTree.insert(38);
-        binarySearchTree.insert(89);
-        binarySearchTree.insert(23);
-        binarySearchTree.insert(12);
-        binarySearchTree.insert(21);
-        binarySearchTree.insert(11);
-        binarySearchTree.insert(85);
-        binarySearchTree.insert(16);
-        binarySearchTree.insert(17);
-        binarySearchTree.insert(14);
+        }
 
 
 
