5cebe7ca2ebde0fd2645e35cd3983827da881e21
Gaurang <gaurang3101@gmail.com>
2020-03-31 20:46:02 +0530

    Problem 5, Student class constructor and fields

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -1,10 +1,15 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.student;
-// to store student information and properties
+
 public class Student {
+    private int rollNo;
+    private String name;
+    private int backLogCounter;
+    private int appearingCount;
+
+    public Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+
 }
