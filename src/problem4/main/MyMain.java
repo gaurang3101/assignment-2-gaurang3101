edc4c4c8b8164632b2aab22ce4befc098cd8d22a
Gaurang <gaurang3101@gmail.com>
2020-03-31 21:07:56 +0530

    problem4, Main class finalized

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,11 +1,20 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:08 PM
- */
 package problem4.main;
+
+import problem2.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        for (int i = 0; i < 11; i++) {
+            tree.insertData((int) (Math.random() * 100));
+        }
+        System.out.println("PreOrder Traversal : ");
+        tree.preOrderTraverse();
+        MyQueue queue = new MyQueue();
+        queue.storePreOrderSuccessor(tree);
+        queue.display();
+    }
 }
