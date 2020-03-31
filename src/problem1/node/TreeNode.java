0c135da96a95f85cc776e8f29bca795c55ed41eb
Gaurang <gaurang3101@gmail.com>
2020-03-31 16:26:35 +0530

    Completed the BST class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,17 +6,15 @@
  */
 package problem1.node;
 
-import java.util.Scanner;
-
 public class TreeNode {
     private int data;
     private TreeNode left;
     private TreeNode right;
 
-    public TreeNode(int data){
-        this.data=data;
-        this.left=null;
-        this.right=null;
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
 
     public int getData() {
