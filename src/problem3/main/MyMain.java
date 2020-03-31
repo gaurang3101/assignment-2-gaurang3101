8c33d1c453bdaa0416e64a8eb2ae3eee0bdc5b83
Gaurang <gaurang3101@gmail.com>
2020-03-31 21:10:47 +0530

    Problem 2, MyMain Class Finalized

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,6 +1,30 @@
-
 package problem3.main;
-// executable class
-// use problem5.student.Student class to create object of student
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue queue = new MyPriorityQueue();
+        queue.enqueue(new Student(8, "Adam"));
+        queue.display();
+        queue.enqueue(new Student(3, "Abhi"));
+        queue.display();
+        queue.enqueue(new Student(1, "Amol"));
+        queue.display();
+        queue.enqueue(new Student(2, "Max"));
+        queue.display();
+        queue.enqueue(new Student(5, "Amit"));
+        queue.display();
+        System.out.println(queue.poll().getStudent() + " polled");
+        queue.display();
+        System.out.println(queue.poll().getStudent() + " polled");
+        queue.display();
+        System.out.println(queue.poll().getStudent() + " polled");
+        queue.display();
+        System.out.println(queue.poll().getStudent() + " polled");
+        queue.display();
+        System.out.println("Que Size: " + queue.getSize());
+
+    }
 }
