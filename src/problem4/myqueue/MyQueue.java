2b27e1edafc82e87c6b97a7e21186f003876664d
Gaurang <gaurang3101@gmail.com>
2020-03-31 21:01:26 +0530

    Problem 4, MyQueue class, Fields and Constructor

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,11 +1,18 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:11 PM
- */
+
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+
+import problem4.node.QueueNode;
+
 public class MyQueue {
+    private QueueNode rear;
+    private QueueNode front;
+    private int size;
+
+    public MyQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
 }
