61c591a1a47a94ffdbb1f7d04339baf3082af418
Gaurang <gaurang3101@gmail.com>
2020-03-31 21:04:49 +0530

    problem4, MyQueue class, Methods defined

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,8 +1,8 @@
-
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
-
+import problem2.mybst.MyBinarySearchTree;
+import problem2.node.TreeNode;
 import problem4.node.QueueNode;
 
 public class MyQueue {
@@ -18,5 +18,89 @@ public class MyQueue {
 
     public int getSize() {
         return size;
+    }
+
+    public void storePreOrderSuccessor(MyBinarySearchTree tree) {
+        TreeNode root = tree.getRoot();
+        storePreOrderSuccesssorRec(root);
+    }
+
+    void storePreOrderSuccesssorRec(TreeNode root) {
+        if (root != null) {
+            enqueue(root.getData());
+            storePreOrderSuccesssorRec(root.getLeft());
+            storePreOrderSuccesssorRec(root.getRight());
+        }
+    }
+
+
+    public boolean isEmpty() {
+        boolean response = false;
+        if (size == 0) {
+            response = true;
+        }
+        return response;
+    }
+
+
+    public void enqueue(int element) {
+        QueueNode node = new QueueNode(element);
+        if (isEmpty()) {
+            rear = node;
+            front = node;
+            size++;
+        } else {
+            rear.setNext(node);
+            rear = node;
+            size++;
+        }
+    }
+
+
+    public QueueNode dequeue() {
+        QueueNode response = null;
+        if (!isEmpty()) {
+            if (front.getNext() != null) {
+                response = front;
+                front = front.getNext();
+                response.setNext(null);
+                size--;
+            } else {
+                response = front;
+                front = null;
+                rear = null;
+                size--;
+            }
+        } else {
+            System.out.println("Queue UnderFlow");
+        }
+        return response;
+
+    }
+
+    public QueueNode peek() {
+        QueueNode response = null;
+        if (!isEmpty()) {
+            response = new QueueNode(front.getData());
+        }
+        return response;
+    }
+
+
+    public void display() {
+        System.out.print("Display Queue: ");
+        QueueNode temp = front;
+        while (temp != null) {
+            System.out.print(temp.getData());
+            if (temp.getNext() != null) {
+                System.out.print("<---");
+            }
+            temp = temp.getNext();
+        }
+        System.out.println();
+    }
+
+    public QueueNode rear() {
+        return rear;
     }
 }
