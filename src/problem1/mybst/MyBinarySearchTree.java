0b2f94a78d43023db38f35508cf9e3a022d5cad4
Gaurang <gaurang3101@gmail.com>
2020-03-31 16:24:59 +0530

    Couldn't complete the generic code in time, so reverting back to an int based BST

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -3,7 +3,8 @@ package problem1.mybst;
 import problem1.node.TreeNode;
 
 // to implement BinarySearchTree
-public class MyBinarySearchTree<E> {
-    private TreeNode<E> root;
+public class MyBinarySearchTree<E extends Comparable<E>> {
+
+
 }
 
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -8,36 +8,38 @@ package problem1.node;
 
 import java.util.Scanner;
 
-public class TreeNode<E> {
-    private TreeNode<E> left;
-    private TreeNode<E> right;
-    private E data;
+public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
 
-    public TreeNode(E data) {
-        this.data = data;
+    public TreeNode(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
     }
 
-    public TreeNode<E> getLeft() {
-        return left;
-    }
-
-    public void setLeft(TreeNode<E> left) {
-        this.left = left;
-    }
-
-    public TreeNode<E> getRight() {
-        return right;
-    }
-
-    public void setRight(TreeNode<E> right) {
-        this.right = right;
-    }
-
-    public E getData() {
+    public int getData() {
         return data;
     }
 
-    public void setData(E data) {
+    public void setData(int data) {
         this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
     }
 }
