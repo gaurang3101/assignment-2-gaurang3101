9a6ff3b2fa93455d873166a6a068ab2531bdff29
Gaurang <gaurang3101@gmail.com>
2020-03-31 21:08:44 +0530

    Problem 2, Node Class Finalized

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,10 +1,30 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
- */
 package problem3.node;
 
+import problem5.student.Student;
+
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
