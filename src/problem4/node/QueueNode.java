47930cc4f7a3718cb231fdbef19a4f79c78ea18b
Gaurang <gaurang3101@gmail.com>
2020-03-31 20:59:31 +0530

    Problem 4, QueueNode class, fields, getter/setter and constructor

---
+++ src/problem4/node/QueueNode.java
@@ -0,0 +1,27 @@
+package problem4.node;
+
+public class QueueNode {
+    private int data;
+    private QueueNode next;
+
+    public QueueNode(int data) {
+        this.data = data;
+        next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public QueueNode getNext() {
+        return next;
+    }
+
+    public void setNext(QueueNode next) {
+        this.next = next;
+    }
+}
