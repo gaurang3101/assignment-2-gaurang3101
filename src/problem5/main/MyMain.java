cafa8046abfd77c4059ee8c2150f9d478369c3e4
Gaurang <gaurang3101@gmail.com>
2020-03-31 22:24:11 +0530

    Final Commit, minor changes

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -10,15 +10,15 @@ public class MyMain {
     public static void main(String[] args) {
         Random rand = new Random();
         MyCircularQueue queue = new MyCircularQueue();
-        queue.enqueue(new Student(1, "Adam", 2, rand.nextInt(2) + 1));
+        queue.enqueue(new Student(11, "Gaurang", 2, rand.nextInt(2) + 1));
         queue.display();
-        queue.enqueue(new Student(2, "Ajay", 4, rand.nextInt(2) + 1));
+        queue.enqueue(new Student(7, "Aryan", 4, rand.nextInt(2) + 1));
         queue.display();
-        queue.enqueue(new Student(3, "Prajjwal", 3, rand.nextInt(2) + 1));
+        queue.enqueue(new Student(9, "Chaitanya", 0, rand.nextInt(2) + 1));
         queue.display();
-        queue.enqueue(new Student(4, "Abhi", 5, rand.nextInt(2) + 1));
+        queue.enqueue(new Student(4, "Nitin", 1, rand.nextInt(2) + 1));
         queue.display();
-        queue.enqueue(new Student(5, "Anand", 0, rand.nextInt(2) + 1));
+        queue.enqueue(new Student(5, "Lucky", 8, rand.nextInt(2) + 1));
         queue.display();
         while (queue.processStudentsList() != false) {
         }
